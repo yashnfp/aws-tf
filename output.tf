2dbc1c2c36cdf39e12426537af94f98bfa207bda
yash87 <yashnfp@gmail.com>
2026-02-09 17:34:01 -0800

    fixing  outputfile

--- output.tf
+++ output.tf
@@ -1,3 +1,9 @@
-output "all_ami_info" {
-  value = data.aws_ami.latest_amazon_linux
+output "instance_public_ip" {
+  value       = aws_instance.app_server.public_ip
+  description = "The public IP of the EC2 instance"
+}
+
+# Fix: Changed 'latest_amazon_linux' to 'amazon_linux' to match main.tf
+output "ami_id_used" {
+  value = data.aws_ami.amazon_linux.id
 }
